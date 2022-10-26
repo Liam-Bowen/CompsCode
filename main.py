519fa3bc29bfdc0b2eae81d08a075d5d5f93a508
Liam-Bowen <57340667+Liam-Bowen@users.noreply.github.com>
2022-10-25 22:41:57 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
-import math
-
-import team
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
-    team.team()
-# def hello():
-# return 'Hello World'
-
-# export FLASK_APP=hello
-# export FLASK_ENV=development
-# flask run
+def my_team():
+    return render_template('home.html')
 
 
-main()
+@app.route('/standings')
+def standings():
+    return render_template('standings.html')
+
+
+@app.route('/scoreboard')
+def scoreboard():
+    return render_template('scoreboard.html')
+
+
+@app.route('/add-players')
+def add_players():
+    return render_template('add_players.html')
+
+
+@app.route('/draft-room')
+def draft_room():
+    return render_template('draft_room.html')
+
+
+@app.route('/my-team')
+def my_team2():
+    return render_template('my_team.html')
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/test2')
+def test2():
+    return render_template('test2.html')
+
+
+app.run(debug=True)
+
